9ba18c9f8082caf1f488dfc7681e8cf01d46b223
dosados <mantsurof93@gmail.com>
2025-09-20 20:24:47 +0300

    first commit

---
+++ hi.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int poem() {
+    printf("poem\n");
+    return 8;
+}
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+
+int main() {
+    int a = poem();
+    printf("%d", a);
+    return 0;
+}
